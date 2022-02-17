539260019fcb2e57bbb1935a902599a30289157f
krollsakh <62526928+krollsakh@users.noreply.github.com>
2022-02-17 19:32:14 +0700

    Initial commit

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <h6>Component Test</h6>
+  <p>Prop msg = {{ msg }}</p>
+  <p>Prop labels = {{ labels }}</p>
+</template>
+
+<script lang="ts" setup>
+
+interface Props {
+  msg?: string
+  labels?: string[]
+}
+
+withDefaults(defineProps<Props>(), {
+  msg: 'hello',
+  label: () => ['one', 'two']
+})
+
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Hello word 1</h1>
+  <test-component />
+</template>
+
+<script lang="ts" setup>
+import TestComponent from '../components/TestComponent.vue'
+</script>
